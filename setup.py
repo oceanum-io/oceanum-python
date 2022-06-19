48e8ec264d643f5cd01da6367a4eb1109daab6b2
dave <d.johnson@oceanum.science>
2022-06-20 08:17:14 +1200

    Add missing h5netcdf requirement

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ requirements = [
     "geopandas",
     "xarray",
     "zarr",
+    "h5netcdf",
     "shapely",
     "orjson",
     "requests",
@@ -67,6 +68,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/oceanum/oceanum-python",
-    version="0.4.4",
+    version="0.4.4-2",
     zip_safe=False,
 )
