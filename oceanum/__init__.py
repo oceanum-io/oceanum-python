820101a28d4244c0bcc6e3a47f2e0b9f184c5f2a
dave <d.johnson@oceanum.science>
2024-02-01 09:47:49 +1300

    Bump version

--- oceanum/__init__.py
+++ oceanum/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Oceanum Developers"""
 __email__ = "developers@oceanum.science"
-__version__ = "0.11.8"
+__version__ = "0.11.9"
 
 
 # Suppress tracebacks in an ipython environment
