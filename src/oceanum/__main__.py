fd26da5e735f64caab4c6274dba80f004f22674f
Andre Lobato <a.lobato@oceanum.science>
2024-09-23 10:03:44 +1200

    Upgrade python versions, fix tox, improve entrypoints

--- src/oceanum/__main__.py
+++ src/oceanum/__main__.py
@@ -9,10 +9,12 @@ from importlib.metadata import entry_points
 from oceanum.cli.main import main
 #from oceanum.cli import auth, datamesh, storage
 
-try:
-    for run_ep in entry_points(group='oceanum.cli'):
+for cli_ep in entry_points(group='oceanum.cli'):
+    cli_ep.load()
+
+for run_ep in entry_points(group='oceanum.cli.run'):
+    try:
         run_ep.load()
-except ModuleNotFoundError as e:
-    click.echo(f'Oceanum Run module {e} not found! Please install oceanum-run-cli package')
-
-
+    except ModuleNotFoundError as e:
+        pass
+        #click.echo(f'Oceanum Run module {e} not found!')
