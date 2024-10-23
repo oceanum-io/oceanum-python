76eda11bc564a51ef732df7a7ff707c9c406bdcc
Andre Lobato <a.lobato@oceanum.science>
2024-10-23 15:48:53 +1300

    Add cli.common.symbols module

---
+++ src/oceanum/cli/common/symbols.py
@@ -0,0 +1,17 @@
+import click
+
+key = click.style(u"\U0001F511", fg='yellow')
+
+watch = click.style("\u23F1", fg='white')
+
+globe = click.style('\U0001F30D', fg='blue')
+
+spin = click.style(u"\u21BB", fg='cyan')
+
+err = click.style('\u2715', fg='red')
+
+chk = click.style('\u2713', fg='green')
+
+wrn = click.style('\u26A0', fg='yellow')
+
+info = click.style('\U0001F6C8', fg='cyan')
