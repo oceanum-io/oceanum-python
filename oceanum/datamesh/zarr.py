c023b9099be289bdceed3e80ec08bca3e363df6a
Sebastien Delaux <s.delaux@oceanum.science>
2025-01-24 21:39:42 +1300

    Adding backslash at the end of the URL is critical

--- oceanum/datamesh/zarr.py
+++ oceanum/datamesh/zarr.py
@@ -130,7 +130,7 @@ class ZarrClient(MutableMapping):
         )
 
     def __iter__(self):
-        resp = self._get(f"{self._proxy}/{self.datasource}")
+        resp = self._get(f"{self._proxy}/{self.datasource}/")
         if not resp:
             return
         ex = re.compile(r"""<(a|A)\s+(?:[^>]*?\s+)?(href|HREF)=["'](?P<url>[^"']+)""")
