fdf11cf44e9b3a3b562cbc77187e872571f2d77b
Andre Lobato <a.lobato@oceanum.science>
2025-02-11 15:44:38 +1300

    Bump on 0.17.0

--- src/oceanum/__init__.py
+++ src/oceanum/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Oceanum Developers"""
 __email__ = "developers@oceanum.science"
-__version__ = "0.17.1"
+__version__ = "0.17.0"
 
 
 # Suppress tracebacks in an ipython environment
