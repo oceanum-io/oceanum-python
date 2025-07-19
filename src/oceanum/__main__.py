9141caadfdca0ab91d2d731ce4fdee2fcb833c0e
Tom Durrant <t.durrant@oceanum.science>
2025-07-20 00:03:39 +1000

    Removed prax entrypoints

--- src/oceanum/__main__.py
+++ src/oceanum/__main__.py
@@ -9,11 +9,8 @@ from importlib.metadata import entry_points
 from oceanum.cli.main import main
 from oceanum.cli import auth, datamesh, storage
 
-# for cli_ep in entry_points(group='oceanum.cli'):
-#     cli_ep.load()
-
-for run_ep in entry_points(group='oceanum.cli.prax'):
+for cli_ep in entry_points(group='oceanum.cli'):
     try:
-        run_ep.load()
+        cli_ep.load()
     except ModuleNotFoundError as e:
         pass
